a6cd31e52f3c7d7804130da0c8106643ea072eb9
liliankavengi <kavengililian14@gmail.com>
2026-05-11 15:57:05 +0300

    initial commit

---
+++ target/debug/deps/bill_manager-5cbdeaccdb4f4609.d
@@ -0,0 +1,5 @@
+C:\Users\kaven\bill-manager\target\debug\deps\bill_manager-5cbdeaccdb4f4609.d: src\main.rs
+
+C:\Users\kaven\bill-manager\target\debug\deps\libbill_manager-5cbdeaccdb4f4609.rmeta: src\main.rs
+
+src\main.rs:
